29f52c6bea88d7d7b34fe3dbdec72e7f650c1c49
canonrebel04 <117294984+canonrebel04@users.noreply.github.com>
2026-07-04 13:31:20 +0000

    🎨 Palette: Add tooltips and placeholders to HUD screens

--- src/main/java/meteordevelopment/meteorclient/systems/hud/screens/HudElementPresetsScreen.java
+++ src/main/java/meteordevelopment/meteorclient/systems/hud/screens/HudElementPresetsScreen.java
@@ -30,7 +30,7 @@ public class HudElementPresetsScreen extends WindowScreen {
         this.x = x + 9;
         this.y = y;
 
-        searchBar = theme.textBox("");
+        searchBar = theme.textBox("", "Search presets...");
         searchBar.action = () -> {
             clear();
             initWidgets();
@@ -60,6 +60,7 @@ public class HudElementPresetsScreen extends WindowScreen {
             l.add(theme.label(preset.title));
 
             WPlus add = l.add(theme.plus()).expandCellX().right().widget();
+            add.tooltip = "Add " + preset.title;
             add.action = () -> {
                 Hud.get().add(preset, x, y);
                 onClose();
