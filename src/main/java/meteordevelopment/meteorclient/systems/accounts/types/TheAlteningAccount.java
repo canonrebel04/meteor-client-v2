dfd44199c8be545fab1d9baed048057971b788ff
canonrebel04 <117294984+canonrebel04@users.noreply.github.com>
2026-07-25 13:51:15 +0000

    Optimize distance calculation in CombatTerrainGrid and fix CI

--- src/main/java/meteordevelopment/meteorclient/systems/accounts/types/TheAlteningAccount.java
+++ src/main/java/meteordevelopment/meteorclient/systems/accounts/types/TheAlteningAccount.java
@@ -36,7 +36,15 @@ public class TheAlteningAccount extends Account<TheAlteningAccount> implements T
     @Override
     public boolean fetchInfo() {
         try {
-            AuthResponse res = authenticate();
+            AuthResponse res = Http.post("https://authserver.thealtening.com/authenticate")
+                    .bodyJson(new AuthRequest(
+                        "Minecraft",
+                        token,
+                        System.getenv().getOrDefault("ALTENING_PASSWORD", "Meteor"),
+                        UUID.randomUUID().toString(),
+                        true
+                    ))
+                    .sendJson(AuthResponse.class);
             if (res == null || res.accessToken == null || res.selectedProfile == null) {
                 MeteorClient.LOG.error("Invalid TheAltening credentials.");
                 return false;
@@ -68,14 +76,7 @@ public class TheAlteningAccount extends Account<TheAlteningAccount> implements T
         }
     }
 
-    private WaybackAuthLib getAuth() {
-        WaybackAuthLib auth = new WaybackAuthLib(ENVIRONMENT.servicesHost());
 
-        auth.setUsername(name);
-        auth.setPassword(System.getenv().getOrDefault("ALTENING_PASSWORD", "Meteor"));
-
-        return auth;
-    }
 
     @Override
     public String getToken() {
