0d50f5135c3231e75e4c5e962f377b1fa18b8c9d
canonrebel04 <117294984+canonrebel04@users.noreply.github.com>
2026-09-17 13:24:52 +0000

    Fix path traversal in BookBot file reading

--- src/main/java/meteordevelopment/meteorclient/systems/modules/misc/BookBot.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/misc/BookBot.java
@@ -188,6 +188,20 @@ public class BookBot extends Module {
                 return;
             }
 
+            try {
+                if (!file.get().toPath().toRealPath().startsWith(MeteorClient.FOLDER.toPath().toRealPath())) {
+                    error("Invalid file path!");
+                    toggle();
+                    return;
+                }
+            } catch (Exception e) {
+                error("Failed to resolve file path.");
+                toggle();
+                return;
+            }
+
+
+
             // Handle the file being empty
             if (file.get().length() == 0) {
                 MutableComponent message = Component.literal("");
